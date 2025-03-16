8a97190c6710db451991c2159ccbd4f4dd41e2b7
kricsleo <kricsleo@163.com>
2025-03-16 17:45:45 +0800

    chore: add playground preview

--- playground/main.ts
+++ playground/main.ts
@@ -1,39 +1,37 @@
-import craie from 'craie'
+import craie from '../src/index'
 
-ouputPreview()
+preview()
 
-function ouputPreview(): void {
-  craie.log(craie.white.bgTeal.roundFull('Preview'))
-  // outputFrontColor()
-  // outputBackgroundColor()
-  // outputRound()
-  // outputModifer()
+function preview(): void {
+  const logs = ['log', 'info', 'warn', 'error']
+
+  const frontColors: string[] = []
+  const bgColors: string[] = []
+  const rounds: string[] = []
+  const modifiers: string[] = []
+
+  for (const alias of Object.keys(craie)) {
+    if (alias.startsWith('bg')) {
+      bgColors.push(alias)
+    }
+    else if (alias.startsWith('round')) {
+      rounds.push(alias)
+    }
+    else if (logs.includes(alias)) {
+      continue
+    }
+    else {
+      modifiers.push(alias)
+    }
+  }
+
+  craie.log(...[
+    ...frontColors,
+    ...bgColors,
+    // @ts-expect-error types
+  ].map(alias => craie[alias](` ${alias} `)), ...[
+    ...rounds,
+    ...modifiers,
+    // @ts-expect-error types
+  ].map(alias => craie.bgRed.white[alias](` ${alias} `)))
 }
-
-// function outputFrontColor() {
-//   const expandedColor = expandColor(color)
-//   const frontColors = Object.keys(expandedColor).filter(color => !color.startsWith('bg'))
-//   // @ts-ignore
-//   const outputs = frontColors.map(alias => craie[alias](` ${alias} `))
-//   craie.log(...outputs)
-// }
-
-// function outputBackgroundColor() {
-//   const expandedColor = expandColor(color)
-//   const backgroundColors = Object.keys(expandedColor).filter(color => color.startsWith('bg'))
-//   // @ts-ignore
-//   const outputs = backgroundColors.map(alias => craie[alias](` ${alias} `))
-//   craie.log(...outputs)
-// }
-
-// function outputRound() {
-//   const rounds = expandRound(round)
-//   // @ts-ignore
-//   const outputs = Object.keys(rounds).map(alias => craie.bgRed.white[alias](` ${alias} `))
-//   craie.log(...outputs)
-// }
-
-// function outputModifer() {
-//   const outputs = Object.keys(modifier).map(alias => craie.bgRed.white[alias](` ${alias} `))
-//   craie.log(...outputs)
-// }
